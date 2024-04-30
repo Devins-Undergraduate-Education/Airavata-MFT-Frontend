8092fe553f6bb6c8c60c68a84ae32b92a6df1618
Devin Fromond <74107804+Dfromond@users.noreply.github.com>
2024-04-29 22:01:41 -0400

    Update list-storages.js

--- pages/list-storages.js
+++ pages/list-storages.js
@@ -1,11 +1,9 @@
-
-import { useEffect, useState } from 'react';
-import
-{
-    Button, Text, Table,
+import React, { useEffect, useState } from 'react';
+import {
+    Button, 
+    Table,
     Thead,
     Tbody,
-    Tfoot,
     Tr,
     Th,
     Td,
@@ -23,13 +21,12 @@ const colorSchemeMapper = {
     "LOCAL": "green",
     "S3": "blue",
 };
-export default function ListStorages()
-{
-    const [dataUse, setDataUse] = useState([{}]);
-    useEffect(() =>
-    {
-        async function getData()
-        {
+
+export default function ListStorages() {
+    const [dataUse, setDataUse] = useState([]);
+
+    useEffect(() => {
+        async function getData() {
             const response = await fetch('http://localhost:5500/list-storages', {
                 method: 'GET',
                 headers: {
@@ -39,28 +36,22 @@ export default function ListStorages()
             const data = await response.json();
             console.log('second page: ', data);
             setDataUse(data.storageList);
-
         }
 
         getData();
-
     }, []);
 
-    const handleDelete = async (id) =>
-    {
+    const handleDelete = (id) => {
         console.log('deleting: ', id);
     };
+
     return (
         <>
             <NavBar />
-
             <Container maxW='container.2xl' p={4}>
                 <Flex justify='space-between' alignItems='center'>
-                    <Heading color='blue.600' size='lg'>List Storages</Heading>
-
-                    <Button bg='blue.400' size='sm' color='white' _hover={{
-                        bg: 'blue.600'
-                    }} as='a' href='/add-storage'>Add Storage</Button>
+                    <Heading color='#2E4660' size='lg'>List Storages</Heading>
+                    <Button bg='#496F97' size='sm' color='white' _hover={{ bg: '#2E4660' }} as='a' href='/add-storage'>Add Storage</Button>
                 </Flex>
                 <TableContainer mt={8}>
                     <Table variant='simple'>
@@ -73,31 +64,20 @@ export default function ListStorages()
                                 <Th>Action</Th>
                             </Tr>
                         </Thead>
-                        <Tbody alignitems='center'>
-                            {
-                                dataUse.map((item, index) =>
-                                {
-                                    return (
-                                        <Tr key={index}>
-                                            <Td>{item.storageName}</Td>
-                                            <Td><Badge colorScheme={colorSchemeMapper[item.storageType]}>{item.storageType}</Badge></Td>
-                                            <Td>{item.storageId}</Td>
-                                            <Td>
-                                                <Flex gap={2}>
-                                                    <Button bg='green.300' size='sm' color='white' _hover={{
-                                                        bg: 'green.600'
-                                                    }} as='a' href={`storage/${item.storageId}?storagetype=${item.storageType}&storagename=${item.storageName}&path=/`}>Open</Button>
-
-                                                    <Button bg='red.300' size='sm' color='white' _hover={{
-                                                        bg: 'red.600'
-                                                    }} onClick={(e) => handleDelete(item.id)}>Delete</Button>
-                                                </Flex>
-                                            </Td>
-                                        </Tr>
-                                    );
-                                }
-                                )
-                            }
+                        <Tbody>
+                            {dataUse.map((item, index) => (
+                                <Tr key={index}>
+                                    <Td>{item.storageName}</Td>
+                                    <Td><Badge colorScheme={colorSchemeMapper[item.storageType]}>{item.storageType}</Badge></Td>
+                                    <Td>{item.storageId}</Td>
+                                    <Td>
+                                        <Flex gap={2}>
+                                            <Button bg='green.300' size='sm' color='white' _hover={{ bg: 'green.600' }} as='a' href={`storage/${item.storageId}?storagetype=${item.storageType}&storagename=${item.storageName}&path=/`}>Open</Button>
+                                            <Button bg='red.300' size='sm' color='white' _hover={{ bg: 'red.600' }} onClick={(e) => handleDelete(item.id)}>Delete</Button>
+                                        </Flex>
+                                    </Td>
+                                </Tr>
+                            ))}
                         </Tbody>
                     </Table>
                 </TableContainer>
